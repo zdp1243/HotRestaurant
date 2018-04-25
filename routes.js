c5f3a2053430115ee00ced9f681b9280f32267d5
unknown <ehurst01@gmail.com>
2018-04-24 19:55:05 -0700

    broke out routes, added post route for reservation

---
+++ routes.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const tableArray = []
+
+module.exports = function(app){
+
+//routes
+
+  
+  app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reservations.html"));
+  });
+
+  app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+
+  app.post('/reserve', (req, res)=>{
+      tableArray.push(req.body)
+      console.log("HIP HIP ARRAY: " + tableArray)
+  });
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+}
