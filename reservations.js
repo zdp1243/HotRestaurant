4a001a99e58c25a4b7e37404af2c25e4f0cfdb55
unknown <brittanycturner@gmail.com>
2018-04-24 19:25:49 -0700

    adding HTML & JS files

---
+++ reservations.js
@@ -0,0 +1,14 @@
+$(".submit").on("click", function(){
+    var newReservation = {
+        customerName: $('#reserve_name').val().trim(),
+        phoneNumber: $('#reserve_phone').val().trim(),
+        customerEmail: $('#reserve_email').val().trim(),
+        customerID: $('#reserve_uniqueID').val().trim()
+    };
+
+ $.post("/api/reservations", newReservation)
+.then(function(data) {
+  console.log("reserations.html", data);
+  alert("Adding reservations...");
+});
+});
